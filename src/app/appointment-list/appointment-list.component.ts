4d1a78cdc33db5dce1b6dda92c8ec2688f611f62
Pika <nguyenvannhut0704@gmail.com>
2025-05-13 15:52:23 +0700

    add interface appointment simply

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppointmentListComponent {
 
+  appointment : Appointment = {
+    id: 1,
+    title : "Petting cat",
+    date : new Date(),
+  }
 }
